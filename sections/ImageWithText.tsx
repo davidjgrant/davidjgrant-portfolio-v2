0427dd12e17ff78e0ecd6aaf86d9d4e149bf5cd7
davidjohngrant <davidgrant132@gmail.com>
2022-08-27 17:31:28 +0100

    margin fix

--- sections/ImageWithText.tsx
+++ sections/ImageWithText.tsx
@@ -7,9 +7,10 @@ import { fade } from '../styles/Animation'
 
 const ImageWithText = ({ featuredBanner: { featuredTitle, featuredDescription }}) => {
   return (
-    <motion.div variants={fade} initial='hidden' whileInView='show' viewport={{ once: true }} className='min-w-full bg-gray-100 dark:bg-zinc-900 h-fit lg:-mx-[20vw] my-16 md:my-32'>
-      <div className='container flex flex-col md:flex-row items-center justify-between'>
-        <div className='text-left max-w-3xl px-8 md:px-4 py-8 mr-auto'>
+    <motion.div variants={fade} initial='hidden' whileInView='show' viewport={{ once: true }}
+      className='min-w-full bg-gray-100 dark:bg-zinc-900 h-fit lg:-mx-[20vw] my-16 md:my-32'>
+      <div className='container flex flex-col md:flex-row items-center justify-between ml-auto'>
+        <div className='text-left max-w-3xl p-8 mr-auto'>
           <h3 className={`font-bold text-lg capitalise text-transparent bg-clip-text purple-purple-gradient`}>{ featuredTitle }</h3>
           <p className='my-4 font-medium'>{ featuredDescription }</p>
         </div>

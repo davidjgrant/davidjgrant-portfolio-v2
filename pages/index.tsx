42c66a798b77ad5daee3688e8a863e9d8dbcafab
davidjgrant <davidgrant132@gmail.com>
2023-10-01 18:07:20 +0100

    homepage profile photo resize mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,38 +24,57 @@ interface Props {
 const Home: NextPage<Props> = ({ projects, home: { jobTitles, profilePic, introPast, introFuture, portfolioTitle, blogTitle, cv } }) => {
   return (
     <Layout>
-        <GradientBanner gradient={'amber-pink-gradient'}>
-          <motion.h1 variants={titleAnimation} initial='hidden' whileInView='show' viewport={{ once: true, amount: 0.8 }}
-            className='my-4 text-3xl sm:text-5xl font-bold capitalize p-3 text-white'>DavidJGrant Portfolio</motion.h1>
-          <JobTitles jobTitles={jobTitles} />
-          <motion.div variants={fade}
-            className="dark:bg-black bg-gray-50 p-4 rounded-full absolute -bottom-20 left-1/2 transform -translate-x-1/2 max-w-[200px] md:max-w-none">
-            <Image
-              className='rounded-full'
-              loader={imageLoader}
-              src={profilePic.url}
-              alt="Profile Pic"
-              width={248}
-              height={248}
-            />
-          </motion.div>
-        </GradientBanner>
-        <article className='container mx-auto'>
-          <IntroSplit text={{ introPast, introFuture, cv }} />
-          <SectionTitle title={'View Portfolio'} arrow={true} color={'amber-pink-gradient'}>
-            { portfolioTitle }
+      <GradientBanner gradient={'amber-pink-gradient'}>
+        <motion.h1
+          variants={titleAnimation}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.8 }}
+          className="my-2 sm:my-4 text-3xl sm:text-5xl font-bold capitalize p-3 text-white"
+        >
+          DavidJGrant Portfolio
+        </motion.h1>
+        <JobTitles jobTitles={jobTitles} />
+        <motion.div
+          variants={fade}
+          className="dark:bg-black bg-gray-50 p-4 rounded-full absolute -bottom-20 left-1/2 transform -translate-x-1/2 max-w-[200px] md:max-w-none w-40 h-40 sm:w-60 sm:h-60"
+        >
+          <Image
+            className="rounded-full"
+            loader={imageLoader}
+            src={profilePic.url}
+            alt="Profile Pic"
+            width={200}
+            height={200}
+          />
+        </motion.div>
+      </GradientBanner>
+      <article className="container mx-auto">
+        <IntroSplit text={{ introPast, introFuture, cv }} />
+        <SectionTitle
+          title={'View Portfolio'}
+          arrow={true}
+          color={'amber-pink-gradient'}
+        >
+          {portfolioTitle}
+        </SectionTitle>
+      </article>
+      <PortfolioGrid projects={projects} />
+      {blogTitle ? (
+        <article className="container mx-auto mb-16">
+          <SectionTitle
+            title={'Recent Insights'}
+            arrow={false}
+            color={'green-blue-gradient'}
+          >
+            {blogTitle}
           </SectionTitle>
         </article>
-        <PortfolioGrid projects={projects} />
-        { blogTitle ?
-          <article className='container mx-auto mb-16'>
-            <SectionTitle title={'Recent Insights'} arrow={false} color={'green-blue-gradient'}>
-              { blogTitle }
-            </SectionTitle>
-          </article>
-        : ''}
+      ) : (
+        ''
+      )}
     </Layout>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
